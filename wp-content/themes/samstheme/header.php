f4e068d508e059c976989ecac1200e58d454957c
Sam O'Connell <sjo773@gmail.com>
2015-12-10 14:55:37 -0600

    styled the header

--- wp-content/themes/samstheme/header.php
+++ wp-content/themes/samstheme/header.php
@@ -2,17 +2,28 @@
 <html lang="en">
 <head>
   <!-- <meta charset="UTF-8"> -->
-  <title>Document</title>
+  <title>Authentify</title>
 
   <?php wp_head(); ?>
 </head>
 <body>
+  <div id="header">
+    <div class="top-header">
+      <div id="logo">
+        <a href="/">Authentify</a>
+      </div>
+      <div id="utility">
+        <a href="#" class="blue-btn">Free Demo</a>
+        <p class="phone">+1.773.243.0300</p>
+      </div>
+    </div>
+    <div id="navigation">  
+        <?php
 
-  <?php
-
-    /*
-    wp_nav_menu(array(
-        'theme_location' => 'nav-menu',
-      ));
-    */
-  ?>
+          wp_nav_menu(array(
+              'theme_location' => 'nav-menu',
+            ));
+         
+        ?>
+    </div>
+  </div>
