5a760a3d8705e9cafe5d6e8b226a9d516973409f
Sam O'Connell <sjo773@gmail.com>
2015-12-10 17:39:45 -0600

    added footer and styled it

--- wp-content/themes/samstheme/footer.php
+++ wp-content/themes/samstheme/footer.php
@@ -1,3 +1,61 @@
+<div id="try-demo">
+  <div class="demo-wrap">
+    <p>Let users authenticate and gain access from anywhere, anytime, from any device.</p>
+    <a href="#" class="blue-btn">Try the Demo</a>
+  </div>  
+</div>
+<div id="footer">
+  <div class="footer-wrap">
+    <div class="footer-logo">
+      <img src="http://authentify.com/wp-content/themes/authentify_childtheme/images/footer-logo.png" alt="Authentify">
+    </div>
+    <div class="contact">
+      <h5>Contact</h5>
+      <p class="address">
+        <strong>Authentify, Inc. Headquarters</strong>
+        <br>
+        8745 West Higgins Road, Suite 240,
+        <br>
+        Chicago, IL 60631
+      </p>
+      <ul>
+        <li class="phone">+1.773.243.0300</li>
+        <li class="fax">+1.773.243.0225</li>
+        <li class="email"><a href="#" target="_blank">info@authentify.com</a></li>
+      </ul>
+    </div>
+    <div class="solutions">
+      <h5>Solutions</h5>
+      <ul>
+        <li>Identity and Access Management</li>
+        <li>Network Security</li>
+        <li>Financial Services</li>
+        <li>e-Commerce</li>
+        <li>Healthcare</li>
+        <li>Education</li>
+      </ul>
+    </div>
+    <div class="learn">
+      <h5>Learn More</h5>
+      <ul>
+        <li>Request Demo Access</li>
+        <li>Interactice Demos</li>
+        <li>Download White Pages</li>
+        <li>View Case Studies</li>
+      </ul>
+    </div>
+    <div class="social">
+      <h5>Follow Us</h5>
+      <ul>
+        <li>Follow Us on Twitter</li>
+        <li>Like Us on Facebook</li>
+        <li>Connect on Linkedin</li>
+        <li>Watch Us on Youtube</li>
+        <li>Read Our Blog</li>
+      </ul>
+    </div>
+  </div>
+</div>
 
   <?php wp_footer(); ?>
 </body>
